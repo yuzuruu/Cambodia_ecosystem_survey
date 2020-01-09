77113fd039498a2e6ebd64835bd51bfc66a936e5
Yuzuru Utsunomiya <35525121+yuzuruu@users.noreply.github.com>
2020-01-09 12:38:23 +0900

    added first

---
+++ living_status_01.r
@@ -0,0 +1,46 @@
+# Working status and ecosystem service at Kampi, Kratie, Cambodia
+# original: 9th. January 2020
+# revised: 
+# by Yuzuru Utsunomiya
+#
+
+# ---- read.library ----
+library(classInt)
+library(DCluster)
+library(dichromat) # color brewer following universal color design
+library(geosphere)
+library(ggmap)
+library(ggsn)
+library(ggrepel)
+library(ggspatial)
+library(gplots)
+library(gpclib)
+library(grid)
+library(lubridate)
+library(maptools)
+library(MASS)
+# library(OpenStreetMap)
+library(raster)
+library(RColorBrewer)
+library(readxl)
+library(rgdal)
+library(sf)
+library(spsurvey)
+library(sp)
+library(spdep)
+library(tidyverse) #
+library(viridis)
+library(viridisLite)
+# library(tidybayes)
+# stan and itssettings
+# library(brms)
+# library(rstan)
+# rstan_options(auto_write = TRUE)
+# options(mc.cores = parallel::detectCores())
+# theme_set(theme_get() + theme(aspect.ratio = 3/4))
+# library(shinystan)
+
+
+
+
+
